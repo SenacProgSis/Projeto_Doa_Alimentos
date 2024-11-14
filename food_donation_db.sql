cf78a93bda19bfb7177cc2945fe551a798aada4f
Meiryelle G. Macedo <meirymacedo.35@gmail.com>
2024-11-14 15:42:06 -0300

    Add files via upload

---
+++ food_donation_db.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE food_donation_db;
+
+USE food_donation_db;
+
+CREATE TABLE pessoa_fisica (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100),
+    telefone VARCHAR(20),
+    localizacao VARCHAR(100)
+);
+
+CREATE TABLE restaurante (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    cnpj VARCHAR(20),
+    endereco VARCHAR(255),
+    telefone VARCHAR(20)
+);
+
+CREATE TABLE ong (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    cnpj VARCHAR(20),
+    responsavel VARCHAR(100),
+    email VARCHAR(100),
+    telefone VARCHAR(20)
+);
