672a6b19b8a56520f6ee3b9cadc02838bcb64d11
Clodomilson <clodomilsonanjos.eng@outlook.com>
2024-11-27 17:24:09 -0300

    "Atualização python"

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash, url_for, session
 from flask_mysqldb import MySQL
-
+# Inicio da Aplicação
 app = Flask(__name__)
 app.secret_key = 'sala12345'
 
