84779b98fd4df334e143eb0f0660ee34e6418e92
Clodomilson <clodomilsonanjos.eng@outlook.com>
2024-12-05 15:31:59 -0300

    SEM SENHA NO BD

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.secret_key = 'nova_senha'
 # Configuração do banco de dados
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'nova_senha'  # Coloque sua senha aqui
+app.config['MYSQL_PASSWORD'] = ''  # Coloque sua senha aqui
 app.config['MYSQL_DB'] = 'doacao_comida'
 
 mysql = MySQL(app)
